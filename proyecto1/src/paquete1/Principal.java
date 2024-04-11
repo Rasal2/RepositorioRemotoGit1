014ce7ad8569c65cd559b9c20d9ba08eac1c8e1f
mañana <maana@A1-23>
2024-04-11 09:39:45 +0200

    Segunda confirmación

--- proyecto1/src/paquete1/Principal.java
+++ proyecto1/src/paquete1/Principal.java
@@ -2,7 +2,10 @@ package paquete1;
 
 public class Principal {
 	public static void main(String[] args) {
+		System.out.println("Estamos haciendo control de versiones con Git"); 
 		System.out.println("Estamos haciendo control de versiones con Git");}
+	
+	    
 		}
 
 
